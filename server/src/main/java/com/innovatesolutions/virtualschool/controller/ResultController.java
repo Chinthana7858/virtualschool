17e04ee1495ff9ebc3a77162220806f6a482487c
REDTECH <rachinthanarajapaksha@gmail.com>
2023-03-28 17:35:11 +0530

    updated result module

--- server/src/main/java/com/innovatesolutions/virtualschool/controller/ResultController.java
+++ server/src/main/java/com/innovatesolutions/virtualschool/controller/ResultController.java
@@ -1,5 +1,6 @@
 package com.innovatesolutions.virtualschool.controller;
 import com.innovatesolutions.virtualschool.entity.Result;
+import com.innovatesolutions.virtualschool.enums.Term;
 import com.innovatesolutions.virtualschool.service.ResultService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,23 +10,23 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping
+@RequestMapping("api/v1/result")
 @AllArgsConstructor
 public class ResultController {
     private final ResultService resultService;
 
-    @GetMapping("api/vi/result/{subjectId}/{classId}/{userid}/{term}")
+    @GetMapping("/{subjectId}/{classId}/{userid}/{term}")
     public ResponseEntity<Result> getResult(
             @PathVariable String subjectId,
             @PathVariable String classId,
             @PathVariable String userid,
-            @PathVariable String term
+            @PathVariable Term term
     ) {
         Result resultRetrieved = resultService.getResult(subjectId, classId, userid, term);
         return new ResponseEntity<>(resultRetrieved, HttpStatus.OK);
     }
 
-    @PostMapping("api/vi/results/{subjectId}/{classId}/{userid}/{term}")
+    @PostMapping("/{subjectId}/{classId}/{userid}/{term}")
     public ResponseEntity<Result> saveResult(@RequestBody Result result) {
         Result savedResult = resultService.saveResult(result);
         return new ResponseEntity<>(savedResult, HttpStatus.CREATED);
--- server/src/main/java/com/innovatesolutions/virtualschool/service/ResultService.java
+++ server/src/main/java/com/innovatesolutions/virtualschool/service/ResultService.java
@@ -1,5 +1,6 @@
 package com.innovatesolutions.virtualschool.service;
 import com.innovatesolutions.virtualschool.entity.Result;
+import com.innovatesolutions.virtualschool.enums.Term;
 import com.innovatesolutions.virtualschool.repository.ResultRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +15,19 @@ public class ResultService {
     @Autowired
     private final ResultRepository resultRepository;
 
-    public Result getResult(String subjectId, String classId, String userid, String term) {
+    public Result getResult(String subjectId, String classId, String userid, Term term) {
         Optional<Result> resultOptional = resultRepository.findBySubjectIdAndClassIdAndUseridAndTerm(subjectId, classId, userid, term);
         return resultOptional.orElse(null);
     }
 
     public Result saveResult(Result result) {
-        Optional<Result> existingResultOptional = resultRepository.findBySubjectIdAndClassIdAndUseridAndTerm(result.getSubjectId(), result.getClassId(), result.getUserid(), result.getTerm());
+        Optional<Result> existingResultOptional = resultRepository.
+                findBySubjectIdAndClassIdAndUseridAndTerm(
+                        result.getSubjectId(),
+                        result.getClassId(),
+                        result.getUserid(),
+                        result.getTerm()
+                );
         if(existingResultOptional.isPresent()){
             Result existingResult = existingResultOptional.get();
             existingResult.setResult(result.getResult());
