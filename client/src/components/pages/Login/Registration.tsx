5317ba975274339af92bccdba0ec0d3bd3ccfec8
Pasinduni Gulawita <68873379+sachiwpg97@users.noreply.github.com>
2023-06-04 20:55:44 +0530

    Add files via upload

--- client/src/components/pages/Login/Registration.tsx
+++ client/src/components/pages/Login/Registration.tsx
@@ -18,17 +18,29 @@ interface ViewLinkProps {
 const ViewLink: React.FC<ViewLinkProps> = ({ url, children }) => (
   <a href={url}>{children}</a>
 );
+interface FormErrors {
+  userRole?:string;
+  fullname?:string;
+  phoneNo?:string;
+  address?:string;
+  dateOfBirth?:string;
+  NIC?:string;
+  stUserid?:string;
+  userid?:string;
+  email?: string;
+  password?: string;
+}
 
 const Registration = () => {
   const [open, setOpen] = useState(true);
   const options = [
     {
-      label: "STUDENT",
-      value: "STUDENT",
-    },
-    {
       label: "PARENT",
       value: "PARENT",
+    },
+    {
+      label: "STUDENT",
+      value: "STUDENT",
     },
     {
       label: "PRINCIPAL",
@@ -50,11 +62,102 @@ const Registration = () => {
   const [stUserid,setstUserid]=useState('');
   const [userid,setuserid]=useState('');
   const [password,setpassword]=useState('');
+  const [errors, setErrors] = useState<FormErrors>({});
+  const [showPassword, setShowPassword] = useState(false);
+
+  const validateForm = () => {
+    let errors: FormErrors = {};
+    let isValid = true;
+    if (!userRole) {
+      errors.userRole = 'User Role is required';
+      isValid = false;
+    }
+    if (!fullname) {
+      errors.fullname = 'Full name is required';
+      isValid = false;
+    }else if (!/^[a-zA-Z ]+$/.test(fullname)) {
+      errors.fullname = 'Invalid email format. Only text characters with spaces are allowed.';
+      isValid = false;
+    }
+    if (!phoneNo) {
+      errors.phoneNo = 'Phone Number is required';
+      isValid = false;
+    }else if (!/^\d{10}$/.test(phoneNo)) {
+      errors.phoneNo = 'Invalid phone number format. Only 10 digits are allowed.';
+      isValid = false;
+    }
+    if (!address) {
+      errors.address = 'Address is required';
+      isValid = false;
+    }
+    if (!dateOfBirth) {
+      errors.dateOfBirth = 'Date Of Birth is required';
+      isValid = false;
+    }else if (!/^\d{4}\/\d{2}\/\d{2}$/.test(dateOfBirth)) {
+      errors.dateOfBirth = 'Invalid date format. Please use the format yyyy/mm/dd.';
+      isValid = false;
+    } else {
+      const parts = dateOfBirth.split('/');
+      const year = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10);
+      const day = parseInt(parts[2], 10);
+      const isValidDate = !isNaN(year) && !isNaN(month) && !isNaN(day) && month >= 1 && month <= 12 && day >= 1 && day <= 31;
+      
+      if (!isValidDate) {
+        errors.dateOfBirth = 'Invalid date. Please enter a valid date.';
+        isValid = false;
+      }
+    }
+
+
+    if(userRole!="STUDENT"){
+      if (!NIC) {
+        errors.NIC = 'NIC is required';
+        isValid = false;
+      }else if(userRole!="STUDENT" && userRole=="PARENT"){
+        if (!stUserid) {
+          errors.stUserid = 'Student Userid is required';
+          isValid = false;
+        }
+      }
+    }
+    
+    if (!userid) {
+      errors.userid = 'userid is required';
+      isValid = false;
+    }
+    // Validate email
+    if (!email) {
+      errors.email = 'Email is required';
+      isValid = false;
+    }else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = 'Invalid email format';
+      isValid = false;
+    }
+
+    // Validate password
+    if (!password) {
+      errors.password = 'Password is required';
+      isValid = false;
+    }else if (password.length < 8) {
+      errors.password = 'Password must be at least 8 characters long';
+      isValid = false;
+    }
+
+    setErrors(errors);
+    return isValid;
+  };
 
   const navigate = useNavigate();
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   const postData = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
+    if (validateForm()) {
+      console.log("came",postData)
     Axios.post('http://localhost:8080/api/vi/users',{
       userRole,
       fullname,
@@ -68,7 +171,7 @@ const Registration = () => {
       password
     }).then((res: any)=>console.log('post data',res)).catch((err: any)=>console.log(err))
   }
-  
+}
   return (
     
      
@@ -89,48 +192,61 @@ const Registration = () => {
                 <option value={option.value}>{option.label}</option>
               ))}
             </select>
+            {errors.userRole && <span style={{color:"#ff0000"}}>{errors.userRole}</span>}
             </div>
           </div>
           <div className="p-3 w-100">
             <div className="form-group p-2">
               <label style={{padding:"5px"}}>Full name</label>
               <input type="text" className="form-control" id="fullName" name="fullName" value={fullname} onChange={(e)=> setFullname(e.target.value)} placeholder="Full name"></input> 
+              {errors.fullname && <span style={{color:"#ff0000"}}>{errors.fullname}</span>}
             </div>
             <div className="form-group p-2">
               <label style={{padding:"5px"}}>Phone number</label>
               <input type="text" className="form-control" id="phoneNo" name="phoneNo" value={phoneNo} onChange={(e)=> setphoneNo(e.target.value)} placeholder="Phone number"></input>
+              {errors.phoneNo && <span style={{color:"#ff0000"}}>{errors.phoneNo}</span>}
              </div>
             <div className="form-group p-2">
               <label style={{padding:"5px"}}>E-mail</label>
               <input type="text" className="form-control" id="email" name="email" value={email} onChange={(e)=> setemail(e.target.value)} placeholder="E-mail"></input>
+              {errors.email && <span style={{color:"#ff0000"}}>{errors.email}</span>}
               <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div className="form-group p-2">
               <label style={{padding:"5px"}}>Address</label>
               <input type="text" className="form-control" id="address" name="address" value={address} onChange={(e)=> setaddress(e.target.value)} placeholder="Address"></input>
+              {errors.address && <span style={{color:"#ff0000"}}>{errors.address}</span>}
             </div>
             <div className="form-group p-2">
               <label style={{padding:"5px"}}>Date of Birth</label>
               {/* <DatePicker onChange={onChange} value={dateOfBirth} id="dateOfBirth" name="dateOfBirth"/> */}
               <input type="text" className="form-control" id="dateOfBirth" name="dateOfBirth" value={dateOfBirth} onChange={(e)=> setdateOfBirth(e.target.value)} placeholder="Date of Birth"></input>
+              {errors.dateOfBirth && <span style={{color:"#ff0000"}}>{errors.dateOfBirth}</span>}
             </div>
           </div>
           <div className="p-3 mr-6 ml-5 w-100">
           {userRole != 'STUDENT' && (
           <><div className="form-group p-2">
-                  <label style={{ padding: "5px" }}>NIC (Students don't need to fill)</label>
+                  <label style={{ padding: "5px" }}>NIC </label>
                   <input type="text" className="form-control" id="NIC" name="NIC" value={NIC} onChange={(e) => setNIC(e.target.value)} placeholder="NIC"></input>
-                </div><div className="form-group p-2">
-                    <label style={{ padding: "5px" }}>Student's User ID (For parents)</label>
-                    <input type="text" className="form-control" id="stUserid" name="stUserid" value={stUserid} onChange={(e) => setstUserid(e.target.value)} placeholder="Student UserID"></input>
+                  {errors.NIC && <span style={{color:"#ff0000"}}>{errors.NIC}</span>}
+                </div></>)}
+          {userRole == 'PARENT' && (
+          <><div className="form-group p-2">
+                    <label style={{ padding: "5px" }}>Student’s Admission number</label>
+                    <input type="text" className="form-control" id="stUserid" name="stUserid" value={stUserid} onChange={(e) => setstUserid(e.target.value)} placeholder="Student’s Admission number"></input>
+                    {errors.stUserid && <span style={{color:"#ff0000"}}>{errors.stUserid}</span>}
                   </div></>)}
             <div className="form-group p-2">
-              <label style={{padding:"5px"}}>User ID</label>
-              <input type="text" className="form-control" id="userid" name="userid" value={userid} onChange={(e)=> setuserid(e.target.value)} placeholder="User ID"></input>
+              <label style={{padding:"5px"}}>Admission Number</label>
+              <input type="text" className="form-control" id="userid" name="userid" value={userid} onChange={(e)=> setuserid(e.target.value)} placeholder="Admission Number"></input>
+              {errors.userid && <span style={{color:"#ff0000"}}>{errors.userid}</span>}
             </div>
             <div className="form-group p-2">
               <label style={{padding:"5px"}}>Password</label>
-              <input type="password" className="form-control" id="password" name="password" value={password} onChange={(e)=> setpassword(e.target.value)} placeholder="Password"></input>
+              <i className={`password-toggle-icon ${showPassword ? 'fa fa-eye-slash' : 'fa fa-eye'}`}
+            onClick={togglePasswordVisibility}
+          ></i><input type={showPassword ? 'password' : 'text'} className="form-control" id="password" name="password" value={password} onChange={(e)=> setpassword(e.target.value)} placeholder="Password"></input>
             </div>
           </div>
         </div>
